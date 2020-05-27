6966dfc232fb21d7884e54af5fb4be74ef4404ba
Laiyi Fu <fulaiyi321@163.com>
2020-05-27 15:47:24 +0800

    Update draw_tsne.R

--- example/PBMCs_dataset/draw_tsne.R
+++ example/PBMCs_dataset/draw_tsne.R
@@ -35,31 +35,6 @@ if (load_tsne_result == TRUE) {
        p=ggplot(pcs$tSNEProj2,aes(x=tSNE1, y=tSNE2, colour=cell_type)) + theme_classic() + geom_point()
        postscript("tsne_PBMCs_dataset.eps")
        print(p)
+       write.csv(pcs$tSNEProj2[,1:2],'Tsne_matrix_PBMCs')
     }
 
-## using Tsne results
-#load("TF_activity_score_PBMCs_pretrained_using_GM.Rdata")
-#load("TF_activity_score_PBMCs_pretrained_using_K562.Rdata")
-#load("TF_activity_score_PBMCs_pretrained_using_H1.Rdata")
-
-
-#all_label = read.table('label_PBMCs_dataset.txt',stringsAsFactors=FALSE)
-
-
-#print("process data...")
-
-#merge_three = cbind(TF_activity_score_PBMCs_pretrained_using_GM,TF_activity_score_PBMCs_pretrained_using_K562,TF_activity_score_PBMCs_pretrained_using_H1)
-
-#scaledData = merge_three
-#pcs = prcomp(scaledData, scale. = F, center = F)
-#pcs$tSNE = tsne(pcs$x[, 1:20])
-#pcs$tSNEProj = data.frame(pcs$tSNE)
-#pcs$tSNEProj2 = cbind(pcs$tSNEProj,all_label)
-#names(pcs$tSNEProj2) = c("tSNE1", "tSNE2","cell_type")
-#print("draw plot...")
-#setEPS()
-#p=ggplot(pcs$tSNEProj2,aes(x=tSNE1, y=tSNE2, colour=cell_type)) + theme_classic() + geom_point()
-#postscript("tsne_PBMCs_dataset.eps")
-#print(p)
-#setEPS()
-#write.csv(pcs$tSNEProj2[,1:2],'Tsne_matrix_PBMCs')
